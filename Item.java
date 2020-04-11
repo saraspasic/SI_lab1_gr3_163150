3482dbecf040a2540abb3e0f13c83a57eacd4485
saraspasic <sara.spasikj@finki.ukim.mk>
2020-04-11 16:13:00 +0200

    tax type variable added

--- Item.java
+++ Item.java
@@ -2,11 +2,13 @@ class Item {
 	int id;
 	String name;
 	double price;
+	
 
 	//TODO add variable.
-
+	double tax;
 	//TODO constructor
-
+	
+	
 	//TODO setters and getters
 
 	double taxReturn () {
