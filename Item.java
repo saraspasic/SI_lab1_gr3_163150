c35558aee573a96a098d1554dab7b992e692c022
saraspasic <sara.spasikj@finki.ukim.mk>
2020-04-12 19:50:29 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -7,11 +7,23 @@ class Item {
 	//TODO add variable.
 	double tax;
 	//TODO constructor
-	
+	public Item(double tax,int id, String name, double price){
+	this.id=id;
+	this.name=name;
+	this.price=price;
+	this.tax=tax;
+	}
 	
 	//TODO setters and getters
+	int getTax(){
+	return tax;
+	}
+	void setTax(double tax){
+	this.tax=tax;
+	}
+	    
 
 	double taxReturn () {
-		//TODO
+		
 	}
 }
